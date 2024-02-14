ff1db965c2a2a68b767660d16c6d6fa652f5cf52
Warren Uhrich <hello@warren.codes>
2024-02-14 16:53:06 -0700

    Add hello 'name' feature to express.js

--- express.js
+++ express.js
@@ -5,3 +5,16 @@ const port = 3000
 app.listen(port, () => {
   console.log('Listening on port 3000');
 })
+
+app.get('/hello/:name', (req, res) => {
+  const name = req.params.name;
+  res.end(`<!DOCTYPE html>
+<html>
+  <head>
+    <title>Hello, ${name}!</title>
+  </head>
+  <body>
+    <h1>Hello, ${name}!</h1>
+  </body>
+</html>`);
+});
