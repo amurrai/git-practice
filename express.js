4c122fd47132b235ab0410183f8b9de478585806
Warren Uhrich <hello@warren.codes>
2024-02-14 16:54:10 -0700

    Add semi-colons

--- express.js
+++ express.js
@@ -1,10 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.listen(port, () => {
   console.log('Listening on port 3000');
-})
+});
 
 app.get('/hello/:name', (req, res) => {
   const name = req.params.name;
